32126cab54bae184612d94433f54ecdcb0461c3f
David Callizaya <davidcallizaya@gmail.com>
2021-05-07 07:37:48 -0400

    Call lambda from script

--- internal/bpmn/ScriptTaskTrait.go
+++ internal/bpmn/ScriptTaskTrait.go
@@ -1,5 +1,14 @@
 package bpmn
 
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
 // ScriptTaskTrait from BPMN
 type ScriptTaskTrait struct {
 }
@@ -12,7 +21,8 @@ func (node *ScriptTask) Init(definitions *Definitions) {
 		if !itIsMe {
 			return
 		}
-		NotifyEvent("RUN_SCRIPT", struct {
+		// Call Lambda Async
+		data := struct {
 			RequestId string
 			TokenId   string
 			Script    string
@@ -20,6 +30,22 @@ func (node *ScriptTask) Init(definitions *Definitions) {
 			RequestId: payload.Token.Instance.RequestId,
 			TokenId:   payload.Token.ID.String(),
 			Script:    node.Script.Body,
-		})
+		}
+		dataEvent, _ := json.Marshal(data)
+		svc := lambda.New(session.New())
+		input := &lambda.InvokeAsyncInput{
+			FunctionName: aws.String("arn:aws:lambda:us-east-2:294911624942:function:TestScript"),
+			InvokeArgs:   aws.ReadSeekCloser(bytes.NewReader(dataEvent)),
+		}
+		svc.InvokeAsync(input)
+		//NotifyEvent("RUN_SCRIPT", struct {
+		//	RequestId string
+		//	TokenId   string
+		//	Script    string
+		//}{
+		//	RequestId: payload.Token.Instance.RequestId,
+		//	TokenId:   payload.Token.ID.String(),
+		//	Script:    node.Script.Body,
+		//})
 	})
 }
